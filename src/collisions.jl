b5c6d25fab52e7656e4131d33b36e2c5685a8ed8
Georgii Oblapenko <kunstmord@kunstmord.com>
2023-11-27 13:12:16 +0100

    Don't need submodules, better structure + exports

--- src/collisions.jl
+++ src/collisions.jl
@@ -1,5 +1,4 @@
-module Collisions
 include("collision_utils.jl")
 include("collision_scattering.jl")
+include("collision_cross_sections.jl")
 include("collision_ntc.jl")
-end
--- src/merging.jl
+++ src/merging.jl
@@ -1,5 +1,12 @@
-module merging
+module Merging
 
-greet() = print("Hello World!")
+include("constants.jl")
+include("particles.jl")
+include("distributions_and_sampling.jl")
+include("physical_props.jl")
+include("collisions.jl")
+
+export load_species_list, Particle, sample_particles_equal_weight!, create_particle_indexer
+export create_props, compute_props!, ParticleIndexer
 
 end # module merging
