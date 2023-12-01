563038afa36b4dc1e0b427c9dcc330c64ab8a3f7
Georgii Oblapenko <kunstmord@kunstmord.com>
2023-12-01 14:34:01 +0100

    Output of moments

--- src/io.jl
+++ src/io.jl
@@ -6,9 +6,13 @@ function create_netcdf_phys_props(nc_filename, phys_props, species_data)
     defDim(ds, "n_cells", phys_props.n_cells)
     defDim(ds, "n_species", phys_props.n_species)
     defDim(ds, "time", Inf)
+    defDim(ds, "n_moments", phys_props.n_moments)
 
     v_spn = defVar(ds, "species_names", String, ("n_species",))
     v_spn[:] = [species.name for species in species_data]
+
+    v_mompows = defVar(ds, "moment_powers", Int64, ("n_moments",))
+    v_mompows[:] = phys_props.moment_powers
 
     v_ndens = defVar(ds, "ndens", Float64, ("n_cells", "n_species", "time"))
     v_vx = defVar(ds, "vx", Float64, ("n_cells", "n_species", "time"))
@@ -16,6 +20,7 @@ function create_netcdf_phys_props(nc_filename, phys_props, species_data)
     v_vz = defVar(ds, "vz", Float64, ("n_cells", "n_species", "time"))
     v_T = defVar(ds, "T", Float64, ("n_cells", "n_species", "time"))
     v_time = defVar(ds, "timestep", Float64, ("time",))
+    v_moments = defVar(ds, "moments", Float64, ("n_cells", "n_species", "n_moments", "time"))
     return ds
 end
 
@@ -30,7 +35,11 @@ function write_netcdf_phys_props(ds, phys_props, timestep)
             ds["vy"][cell, species, currtimesteps] = phys_props.v[cell, species, 2]
             ds["vz"][cell, species, currtimesteps] = phys_props.v[cell, species, 3]
             ds["T"][cell, species, currtimesteps] = phys_props.T[cell, species, 1]
-
+            # for mom_id in 1:phys_props.n_moments
+            if (phys_props.n_moments > 0)
+                ds["moments"][cell, species, :, currtimesteps] = phys_props.moments[cell, species, :]
+            end
+            # end
         end
     end
     # println(currtimesteps)
