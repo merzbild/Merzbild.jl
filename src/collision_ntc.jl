7ee62826687d3fb2e99e878ff12177e616ddc2a6
Georgii Oblapenko <kunstmord@kunstmord.com>
2024-01-02 17:05:45 +0100

    Fixed 1/2 factor in single-species, started 2-species NTC

--- src/collision_ntc.jl
+++ src/collision_ntc.jl
@@ -11,10 +11,23 @@ function create_collision_factors()
     return CollisionFactors(0, 0.0, 0.0, 0, 0, 0)
 end
 
-# TODO: version to create an array for each species pair
+function create_collision_factors(n_species)
+    coll_factor_array = Array{CollisionFactors}(undef, (n_species, n_species))
+    for k in 1:n_species
+        for i in 1:n_species
+            coll_factor_array[i,k] = CollisionFactors(0, 0.0, 0.0, 0, 0, 0)
+        end
+    end
+    return coll_factor_array
+end
 
 function compute_n_coll_single_species(rng, collision_factors, particle_indexer, Δt, V)
-    return Δt * particle_indexer.n_total * (particle_indexer.n_total - 1) * collision_factors.sigma_g_w_max / V +
+    return 0.5 * Δt * particle_indexer.n_total * (particle_indexer.n_total - 1) * collision_factors.sigma_g_w_max / V +
+        rand(rng, Float64)
+end
+
+function compute_n_coll_two_species(rng, collision_factors, particle_indexer_1, particle_indexer_2, Δt, V)
+    return Δt * particle_indexer_1.n_total * particle_indexer_2.n_total * collision_factors.sigma_g_w_max / V +
         rand(rng, Float64)
 end
 
@@ -91,6 +104,84 @@ function ntc_single_species!(rng, collision_factors, particle_indexer, collision
                     particles[n_total].x = particles[k].x
                 end
                 scatter_vhs(rng, collision_data, interaction, particles[i], particles[k])
+            end
+        end
+    end
+end
+
+
+
+function ntc_two_species!(rng, collision_factors, particle_indexer_1, particle_indexer_2,
+    collision_data, interaction, particles_1, particles_2,
+    Δt, V)
+    # compute ncoll
+    # loop over particles
+    # update sigma_g_w_max
+    # collide
+    collision_factors.n1 = particle_indexer_1.n_total
+    collision_factors.n2 = particle_indexer_2.n_total
+    n_coll_float = compute_n_coll_two_species(rng, collision_factors,
+    particle_indexer_1, particle_indexer_2, Δt, V)
+    n_coll_int = floor(Int64, n_coll_float)
+    println(n_coll_float, ", ", n_coll_int)
+
+    collision_factors.n_coll = n_coll_int
+    collision_factors.n_coll_performed = 0
+    collision_factors.n_eq_w_coll_performed = 0
+
+    for _ in 1:n_coll_int
+        # TODO: check if 0 or 1 !!!
+        i = floor(Int64, rand(rng, Float64) * particle_indexer_1.n_total)
+        k = floor(Int64, rand(rng, Float64) * particle_indexer_2.n_total)
+
+        # example: bounds from [1,4], [7,9]; n_total = 7
+        # n_group1 = 4, n_group2 = 3
+        # i = 0,1,2,3 - [1,4]
+        # i = 4,5,6 - [7,9]
+        i = map_cont_index(particle_indexer_1, i)
+        k = map_cont_index(particle_indexer_2, k)
+        
+        compute_g(collision_data, particles_1[i], particles_2[k])
+
+        if (collision_data.g > eps())
+
+            sigma = sigma_vhs(interaction, collision_data.g)
+            sigma_g_w_max = sigma * collision_data.g * max(particles_1[i].w, particles_2[k].w)
+
+            # update (σ g w)_max if needed
+            collision_factors.sigma_g_w_max = sigma_g_w_max > collision_factors.sigma_g_w_max ? sigma_g_w_max : collision_factors.sigma_g_w_max
+            
+            if (rand(rng, Float64) < sigma_g_w_max / collision_factors.sigma_g_w_max)
+                collision_factors.n_coll_performed += 1
+                compute_com(collision_data, interaction, particles_1[i], particles_2[k])
+
+                # do collision
+                if (particles[i].w == particles[k].w)
+                    collision_factors.n_eq_w_coll_performed += 1
+                    scatter_vhs(rng, collision_data, interaction, particles_1[i], particles_2[k])
+                elseif (particles_1[i].w > particles_2[k].w)
+                    # we split particle i, update velocity of i and k (split part remains unchanged)
+
+                    # first need to update the particle indexer struct
+                    update_particle_indexer_new_particle(particle_indexer)
+
+                    Δw = particles_1[i].w - particles_2[k].w
+                    particles_1[i].w = particles_2[k].w
+
+                    particles_1[n_total].w = Δw
+                    particles_1[n_total].v = particles_1[i].v
+                    particles_1[n_total].x = particles_1[i].x
+                else  # (particles[k].w > particles[i].w)
+                    update_particle_indexer_new_particle(particle_indexer)
+
+                    Δw = particles_2[k].w - particles_1[i].w
+                    particles_2[k].w = particles_1[i].w
+
+                    particles_2[n_total].w = Δw
+                    particles_2[n_total].v = particles_2[k].v
+                    particles_2[n_total].x = particles_2[k].x
+                end
+                scatter_vhs(rng, collision_data, interaction, particles_1[i], particles_2[k])
             end
         end
     end
