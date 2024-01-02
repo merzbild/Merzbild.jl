1bed9014052fced472441f07c5677107eab34bb4
Georgii Oblapenko <kunstmord@kunstmord.com>
2024-01-02 17:06:01 +0100

    Some structs for tabulated CS data

--- src/collision_cross_sections.jl
+++ src/collision_cross_sections.jl
@@ -1,3 +1,27 @@
+struct TabulatedCSData
+    E::Vector{Float64}
+    sigma::Vector{Float64}
+    ΔE::Float64  # how much energy is lost / threshold energy
+    species_loss::Int8  # which species loses energy
+end
+
+@enum ScatteringLaw Isotropic=1 Okhrimovskyy=2
+@enum ElectronEnergySplit Equal=1 ZeroE=2 # Tanh law
+struct Ionization
+    data::TabulatedCSData
+    scattering::ScatteringLaw
+    split::ElectronEnergySplit
+end
+struct ElasticScattering
+    data::TabulatedCSData
+    scattering::ScatteringLaw
+end
+struct ExcitationSink
+    data::Vector{TabulatedCSData}
+    scattering::ScatteringLaw
+end
+
 function sigma_vhs(interaction, g)
     return interaction.vhs_factor * g^(1.0 - 2 * interaction.vhs_o)
 end
+
