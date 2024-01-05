8c6fc09b208b4d0ea61d0f321699009a6c9380fd
Georgii Oblapenko <kunstmord@kunstmord.com>
2024-01-05 12:30:13 +0100

    New ordering of output arrays, vel in single array

--- src/io.jl
+++ src/io.jl
@@ -3,6 +3,7 @@ using NCDatasets
 function create_netcdf_phys_props(nc_filename, phys_props, species_data)
     ds = Dataset(nc_filename, "c")
 
+    defDim(ds, "vel_components", 3)
     defDim(ds, "n_cells", phys_props.n_cells)
     defDim(ds, "n_species", phys_props.n_species)
     defDim(ds, "time", Inf)
@@ -15,12 +16,13 @@ function create_netcdf_phys_props(nc_filename, phys_props, species_data)
     v_mompows[:] = phys_props.moment_powers
 
     v_ndens = defVar(ds, "ndens", Float64, ("n_cells", "n_species", "time"))
-    v_vx = defVar(ds, "vx", Float64, ("n_cells", "n_species", "time"))
-    v_vy = defVar(ds, "vy", Float64, ("n_cells", "n_species", "time"))
-    v_vz = defVar(ds, "vz", Float64, ("n_cells", "n_species", "time"))
+    v_v = defVar(ds, "v", Float64, ("vel_components", "n_cells", "n_species", "time"))
+    # v_vx = defVar(ds, "vx", Float64, ("n_cells", "n_species", "time"))
+    # v_vy = defVar(ds, "vy", Float64, ("n_cells", "n_species", "time"))
+    # v_vz = defVar(ds, "vz", Float64, ("n_cells", "n_species", "time"))
     v_T = defVar(ds, "T", Float64, ("n_cells", "n_species", "time"))
     v_time = defVar(ds, "timestep", Float64, ("time",))
-    v_moments = defVar(ds, "moments", Float64, ("n_cells", "n_species", "n_moments", "time"))
+    v_moments = defVar(ds, "moments", Float64, ("n_moments", "n_cells", "n_species", "time"))
     return ds
 end
 
@@ -31,13 +33,11 @@ function write_netcdf_phys_props(ds, phys_props, timestep)
     for species in 1:phys_props.n_species
         for cell in 1:phys_props.n_cells
             ds["ndens"][cell, species, currtimesteps] = phys_props.n[cell, species]
-            ds["vx"][cell, species, currtimesteps] = phys_props.v[cell, species, 1]
-            ds["vy"][cell, species, currtimesteps] = phys_props.v[cell, species, 2]
-            ds["vz"][cell, species, currtimesteps] = phys_props.v[cell, species, 3]
+            ds["v"][:, cell, species, currtimesteps] = phys_props.v[:,cell, species]
             ds["T"][cell, species, currtimesteps] = phys_props.T[cell, species, 1]
-            # for mom_id in 1:phys_props.n_moments
+
             if (phys_props.n_moments > 0)
-                ds["moments"][cell, species, :, currtimesteps] = phys_props.moments[cell, species, :]
+                ds["moments"][:, cell, species, currtimesteps] = phys_props.moments[:,cell, species]
             end
             # end
         end
