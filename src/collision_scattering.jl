824780eac640aca1e9ea2adf3bcaede009b8aca1
Georgii Oblapenko <kunstmord@kunstmord.com>
2024-01-05 12:27:45 +0100

    Faster via SVector ops

--- src/collision_scattering.jl
+++ src/collision_scattering.jl
@@ -1,15 +1,13 @@
 function scatter_vhs(rng, collision_data, interaction, p1, p2)
-    ϕ = 2.0 * π * rand(rng, Float64)
+    ϕ = twopi * rand(rng, Float64)
     cphi = cos(ϕ)
     sphi = sin(ϕ)
 
     ctheta = 2.0 * rand(rng, Float64) - 1.0
     stheta = sqrt(1.0 - ctheta^2)
 
-    collision_data.g_vec_new[1] = collision_data.g * stheta * cphi
-    collision_data.g_vec_new[2] = collision_data.g * stheta * sphi
-    collision_data.g_vec_new[3] = collision_data.g * ctheta
+    collision_data.g_vec_new = collision_data.g * SVector{3,Float64}(stheta * cphi, stheta * sphi, ctheta)
 
-    p1.v = collision_data.v_com .+ interaction.μ2 * collision_data.g_vec_new
-    p2.v = collision_data.v_com .- interaction.μ1 * collision_data.g_vec_new
+    p1.v = collision_data.v_com + interaction.μ2 * collision_data.g_vec_new
+    p2.v = collision_data.v_com - interaction.μ1 * collision_data.g_vec_new
 end
