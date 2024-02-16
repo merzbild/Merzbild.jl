220ebea462aeb870ff8a956b7b5eacf473bd3203
Georgii Oblapenko <kunstmord@kunstmord.com>
2024-02-16 13:10:15 +0100

    Output # of particles and particle array sizes

--- src/io.jl
+++ src/io.jl
@@ -15,11 +15,10 @@ function create_netcdf_phys_props(nc_filename, phys_props, species_data)
     v_mompows = defVar(ds, "moment_powers", Int64, ("n_moments",))
     v_mompows[:] = phys_props.moment_powers
 
+    v_lpa = defVar(ds, "length_particle_array", Float64, ("n_species", "time"))
+    v_np = defVar(ds, "np", Float64, ("n_cells", "n_species", "time"))
     v_ndens = defVar(ds, "ndens", Float64, ("n_cells", "n_species", "time"))
     v_v = defVar(ds, "v", Float64, ("vel_components", "n_cells", "n_species", "time"))
-    # v_vx = defVar(ds, "vx", Float64, ("n_cells", "n_species", "time"))
-    # v_vy = defVar(ds, "vy", Float64, ("n_cells", "n_species", "time"))
-    # v_vz = defVar(ds, "vz", Float64, ("n_cells", "n_species", "time"))
     v_T = defVar(ds, "T", Float64, ("n_cells", "n_species", "time"))
     v_time = defVar(ds, "timestep", Float64, ("time",))
     v_moments = defVar(ds, "moments", Float64, ("n_moments", "n_cells", "n_species", "time"))
@@ -32,6 +31,8 @@ function write_netcdf_phys_props(ds, phys_props, timestep)
     ds["timestep"][currtimesteps] = timestep
     for species in 1:phys_props.n_species
         for cell in 1:phys_props.n_cells
+            ds["length_particle_array"][species, currtimesteps] = phys_props.lpa[species]
+            ds["np"][cell, species, currtimesteps] = phys_props.np[cell, species]
             ds["ndens"][cell, species, currtimesteps] = phys_props.n[cell, species]
             ds["v"][:, cell, species, currtimesteps] = phys_props.v[:,cell, species]
             ds["T"][cell, species, currtimesteps] = phys_props.T[cell, species, 1]
