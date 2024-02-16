2be7875915f9ae046ea9f4084efea14c10cabd81
Georgii Oblapenko <kunstmord@kunstmord.com>
2024-02-16 16:29:50 +0100

    Test of grid-based initialization

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ using SpecialFunctions
 
 include("test_computes.jl")  # first we test functions that compute physical properties
 include("test_sampling.jl")  # then we test sampling functions
+include("test_grid_sampling.jl")  # then we test grid sampling functions
 include("test_collision_utils.jl")  # then we test some collision utilities
 include("test_bkw.jl")  # then we test the BKW relaxation case against reference solutions
 include("test_2species.jl")  # then we test 2-species elastic collisions
---
+++ test/test_grid_sampling.jl
@@ -0,0 +1,37 @@
+@testset "grid_sampling" begin
+
+    species_list::Vector{Species} = load_species_list("data/particles.toml", "Ar")
+
+    seed = 1234
+    Random.seed!(seed)
+    rng::Xoshiro = Xoshiro(seed)
+
+    n_particles = 50000
+
+    n_dens = 1e20
+    Δabs = 0.5
+    Δrel_small = 1e-2
+    Δrel_xsmall = 1e-13
+    nv = 20
+
+    Fnum::Float64 = n_dens / n_particles
+
+    for (v0, T0) in zip([[0.0, 0.0, 0.0], [20.0, -10.0, 30.0], [3000.0, 2000.0, -1000.0]], [273.0, 1000.0, 500.0])
+        particles::Vector{Vector{Particle}} = [Vector{Particle}(undef, n_particles)]
+
+        n_sampled = sample_maxwellian_on_grid!(rng, particles[1], nv, T0, species_list[1].mass, n_dens,
+        0.0, 1.0, 0.0, 1.0, 0.0, 1.0; v_mult=3.5, cutoff_mult=3.5, noise=0.0, v_offset=v0)
+        
+        particle_indexer::Array{ParticleIndexer, 2} = Array{ParticleIndexer, 2}(undef, 1, 1)
+        particle_indexer[1,1] = create_particle_indexer(n_sampled)
+
+        phys_props::PhysProps = create_props(1, 1, [], Tref=1)
+        phys_props_no_moments::PhysProps = create_props(1, 1, [], Tref=1)
+        compute_props!(phys_props, particle_indexer, particles, species_list)
+        @test abs((phys_props.n[1,1] - n_dens) / n_dens) < Δrel_xsmall
+        @test abs((phys_props.v[1,1,1] - v0[1])) < Δabs
+        @test abs((phys_props.v[2,1,1] - v0[2])) < Δabs
+        @test abs((phys_props.v[3,1,1] - v0[3])) < Δabs
+        @test abs((phys_props.T[1,1] - T0) / T0) < Δrel_small
+    end
+end
