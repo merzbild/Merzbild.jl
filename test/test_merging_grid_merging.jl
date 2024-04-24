4d3474dc6f04a710492b68412148f6e6eef7fab2
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2024-04-24 15:35:07 +0200

    Grid-based merging test

--- test/test_merging_grid_merging.jl
+++ test/test_merging_grid_merging.jl
@@ -15,10 +15,10 @@
     Δabs = 2.5
     Δrel_xsmall = 5e-13
 
-    n_particles = 40000
+    n_particles = 5000
     particles::Vector{Vector{Particle}} = [Vector{Particle}(undef, n_particles)]
     T0 = 300.0
-    Fnum = 1e20
+    Fnum = 1e8
     vx0 = 2000.0
     vy0 = 500.0
     vz0 = -400.0
@@ -28,8 +28,25 @@
     particle_indexer = create_particle_indexer_array(n_particles)
 
     compute_props!(phys_props, particle_indexer, particles, species_list)
+
+    n0_computed = phys_props.n[1,1]
+    np0_computed = phys_props.np[1,1]
+    T0_computed = phys_props.T[1,1]
+    v0_computed = phys_props.v[:,1,1]
+
     mg = create_merging_grid(Nx, Ny, Nz, 3.5)
 
     Merzbild.compute_velocity_extent!(1, 1, mg, phys_props, species_list)
     merge_grid_based!(1, 1, mg, phys_props, species_list, particles, particle_indexer)
+
+    @test particle_indexer.n_total[1] < n_particles
+    @test particle_indexer.n_total[1] == particle_indexer.indexer[1,1].n_local
+
+    compute_props!(phys_props, particle_indexer, particles, species_list)
+    @test particle_indexer.n_total[1] == phys_props.np[1,1]
+    @test abs(phys_props.n[1,1] - n0_computed) < eps()
+    @test abs(phys_props.T[1,1] - T0_computed) < 3e-12
+    @test abs(v0_computed[1] - phys_props.v[1,1,1]) < 3e-12
+    @test abs(v0_computed[2] - phys_props.v[2,1,1]) < 3e-12
+    @test abs(v0_computed[3] - phys_props.v[3,1,1]) < 3e-12
 end
