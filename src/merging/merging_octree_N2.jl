fba6df231c64ee1cd31b0fae1e0e6c654ed8f940
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2024-04-30 15:53:45 +0200

    Some basic struct additions for octree N:2 merging

--- src/merging/merging_octree_N2.jl
+++ src/merging/merging_octree_N2.jl
@@ -1,7 +1,19 @@
 using StaticArrays
 
-mutable struct Octree
+@enum OctreeMergeSplit MidSplit=1 MeanSplit=2 MedianSplit=3
+
+# struct for N:2 merge
+mutable struct OctreeN2
     max_Nbins::Int64
     Nbins::Int64
     bins::GridCell
+
+    # particles in bins[i] have indices in particle_index_buffer[bin_start[i]:bin_end[i]]
+    bin_start::Vector{Int64}
+    bin_end::Vector{Int64}
+
+    # this stores sorted particle indices
+    particle_index_buffer::Vector{Int64}
+
+    split::OctreeMergeSplit
 end
