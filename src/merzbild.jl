89b999b6960383c0c9cb972394d70f4799440fee
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2024-05-08 17:13:56 +0200

    export octree-related stuff

--- src/merzbild.jl
+++ src/merzbild.jl
@@ -8,7 +8,8 @@ include("collisions.jl")
 include("io.jl")
 include("merging/merging.jl")
 
-DELTA_PARTICLES::Int32 = 256
+const OCTREE_DEFAULT_BUFFER_SIZE::Int32 = 8192
+const DELTA_PARTICLES::Int32 = 256
 
 export sample_maxwellian_on_grid!, sample_on_grid!, bkw, maxwellian
 export load_species_list, Particle, sample_particles_equal_weight!, create_particle_indexer, create_particle_indexer_array
@@ -23,5 +24,8 @@ export estimate_sigma_g_w_max, estimate_sigma_g_w_max!
 export ntc!
 export k_B
 export create_merging_grid, merge_grid_based!
+export OctreeBinMidSplit, OctreeBinMeanSplit, OctreeBinMedianSplit
+export  OctreeInitBinMinMaxVel, OctreeInitBinMinMaxVelSym, OctreeInitBinC
+export create_merging_octree, merge_octree_N2_based!
 
 end # module merzbild
