fb353659e9c51ee8d6dc50364a1dd6e012ea3419
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2024-05-10 15:04:53 +0200

    More sorting tests

--- test/test_octree_sorting.jl
+++ test/test_octree_sorting.jl
@@ -47,14 +47,20 @@
         return vp
     end
 
-    function create_7particles(missing_octant)
+    function create_7particles(missing_octant, reverse)
         # create 1 particle per octant, except one empty octant
         
         vp = Vector{Particle}(undef, 7)
 
         i = 1
 
-        for octant in 8:-1:1 # iterate in reverse so that we actually have to sort the particles
+        if reverse
+            rr = 8:-1:1 # iterate in reverse so that we actually have to sort the particles
+        else
+            rr = 1:8
+        end
+
+        for octant in rr
             if octant != missing_octant
                 vp[i] = create_particle_in_octant(octant, 1.0)
                 i += 1
@@ -70,6 +76,7 @@
     Random.seed!(seed)
     rng::Xoshiro = Xoshiro(seed)
 
+    # first we test function that computes octants
     vmid = SVector{3, Float64}(-1.0, -1.0, -1.0)
     
     v0 = SVector{3, Float64}(-2.0, -2.0, -2.0)  # - - -
@@ -96,6 +103,7 @@
     v0 = SVector{3, Float64}(2.0, 2.0, 2.0)  # + + +
     @test Merzbild.compute_octant(v0, vmid) == 8
 
+    # then we test particle sorting with 2 particles in a specific octant
     # octants are 8, 7, 6, 5, 4, 3, 3, 2, 1
     particles9::Vector{Vector{Particle}} = [create_9particles(3, true)]
     pia = create_particle_indexer_array(9)
@@ -107,6 +115,7 @@
     end
     
     Merzbild.split_bin!(octree, 1, particles9[1])
+    @test maximum(abs.(octree.vel_middle)) < 1e-12  # should be almost 0 since everything is symmetric
     @test octree.Nbins == 8
     for i in 1:9
         @test i in octree.particle_indexes_sorted[1:9]
@@ -115,8 +124,6 @@
     for i in 1:9
         @test octree.particle_indexes_sorted[i] == 9 - i + 1
     end
-
-    # TODO: bin starts/ends
 
     # more convoluted test case for the sorting
     # particles:  1, 2, 3, 4, 5, 6, 7, 8, 9
@@ -127,12 +134,48 @@
     Merzbild.init_octree!(1, 1, octree, pia)
     Merzbild.split_bin!(octree, 1, particles9[1])
     @test octree.Nbins == 8
-    for i in 1:9
-        @test i in octree.particle_indexes_sorted[1:9]
-    end
     expected = [9, 8, 7, 6, 1, 2, 3, 4, 5]
-    
     @test octree.particle_indexes_sorted[1:9] == expected
 
-    # TODO: try the same but with pia constructed by hand with 2 split
+    @test octree.bin_start[1:8] == [1, 2, 3, 5, 6, 7, 8, 9]
+    @test octree.bin_end[1:8] == [1, 2, 4, 5, 6, 7, 8, 9]
+
+    # the same but with pia constructed by hand with particle indices split into 2 groups
+    pia2 = create_particle_indexer_array(3)
+    pia2.n_total[1] = 9
+    pia2.indexer[1,1].n_local = 9
+
+    pia2.indexer[1,1].start1 = 1
+    pia2.indexer[1,1].end1 = 3
+    pia2.indexer[1,1].n_group1 = 3
+    
+    pia2.indexer[1,1].start2 = 4
+    pia2.indexer[1,1].end2 = 9
+    pia2.indexer[1,1].n_group2 = 6
+
+    Merzbild.init_octree!(1, 1, octree, pia2)
+    Merzbild.split_bin!(octree, 1, particles9[1])
+
+    @test octree.Nbins == 8
+    expected = [9, 8, 7, 6, 1, 2, 3, 4, 5]
+    @test octree.particle_indexes_sorted[1:9] == expected
+    @test octree.bin_start[1:8] == [1, 2, 3, 5, 6, 7, 8, 9]
+    @test octree.bin_end[1:8] == [1, 2, 4, 5, 6, 7, 8, 9]
+
+    # now we test for 7 particles, i.e. with one empty octree bin
+    # particles:  1, 2, 3, 4, 5, 6, 7
+    # octants are 8, 7, 6, 4, 3, 2, 1
+    particles7::Vector{Vector{Particle}} = [create_7particles(5, true)]
+    pia3 = create_particle_indexer_array(7)
+
+    Merzbild.init_octree!(1, 1, octree, pia3)
+    Merzbild.split_bin!(octree, 1, particles7[1])
+    @test octree.Nbins == 7
+    expected = [7, 6, 5, 4, 3, 2, 1]
+    @test octree.particle_indexes_sorted[1:7] == expected
+    @test octree.bin_start[1:7] == [1, 2, 3, 4, 5, 6, 7]
+    @test octree.bin_end[1:7] == [1, 2, 3, 4, 5, 6, 7]
+
+    # TODO: test double-splitting, i.e. we have 1 p/bin in 7 bins, the 8th bin has 4 particles
+    # we do splitting of the main 0 bin, and then do a second split
 end
