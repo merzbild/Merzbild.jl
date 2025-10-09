490d9d8a73a07a3d9af2b073ff45592804d9e07d
Georgii Oblapenko <kunstmord@kunstmord.com>
2025-10-09 12:28:50 +0200

    added possibility to do multiple files + labels

--- scripts/plot_1D.py
+++ scripts/plot_1D.py
@@ -1,6 +1,6 @@
 # Usage example:
 # python scripts/plot_1D.py --file scratch/data/couette_0.0005_50_500.0_300.0_1000.nc --propname T --startt 1
-# --plotname plots/couette_T.png
+# --plotname plots/couette_T.png --labels "Couette"
 # velocity is plotted with an offset of a linear profile that goes from -500 to 500 m/s
 
 from matplotlib import pyplot as plt
@@ -9,10 +9,11 @@ from netCDF4 import Dataset
 import numpy as np
 
 parser = argparse.ArgumentParser(description='Plot data from a 1D single-species simulation')
-parser.add_argument("--file", required=True)
+parser.add_argument("--files", nargs='+', required=True)
 parser.add_argument("--propname", required=True)
 parser.add_argument("--plotname", required=True)
 parser.add_argument("--startt", required=True)
+parser.add_argument("--labels", nargs='+')
 args = parser.parse_args()
 
 
@@ -35,36 +36,44 @@ v_map = {
     "v_z": 2,
 }
 
-ds = Dataset(args.file)
+labels = args.labels
+if labels == None:
+    labels = args.files
+elif len(labels) != len(args.files):
+    raise ValueError("Length of labels list should be the same as of the files' list!")
+
 startt = int(args.startt)
 
-if propname in ["ndens", "np", "T"]:
-    nx = np.shape(ds.variables[propname][:].data)[-1]
-    nt = np.shape(ds.variables[propname][:].data)[0]
-    x_arr = np.linspace(1, nx, nx)
+for label, file in zip(labels, args.files):
+    ds = Dataset(file)
 
-    data_arr = np.asarray(ds.variables[propname][:].data[:, 0, :])
-    for i in range(startt, nt):
-        ax.plot(x_arr, data_arr[i, :], label=f"nt={i}", linewidth=2)
-elif propname.lower() in v_map:
-    nx = np.shape(ds.variables["v"][:].data)[-2]
-    nt = np.shape(ds.variables["v"][:].data)[0]
-    x_arr = np.linspace(1, nx, nx)
+    if propname in ["ndens", "np", "T"]:
+        nx = np.shape(ds.variables[propname][:].data)[-1]
+        nt = np.shape(ds.variables[propname][:].data)[0]
+        x_arr = np.linspace(1, nx, nx)
 
-    data_arr = np.asarray(ds.variables["v"][:].data[:, 0, :, v_map[propname]])
-    voffset = np.linspace(-500, 500, nx)
-    for i in range(startt,nt):
-        ax.plot(x_arr, data_arr[i, :] - voffset, label=f"nt={i}", linewidth=2)
-    # ax.plot(ds.variables["timestep"][:].data, ds.variables["v"][:].data[:, 0, 0, v_map[propname]], label=label, linewidth=2)
+        data_arr = np.asarray(ds.variables[propname][:].data[:, 0, :])
+        for i in range(startt, nt):
+            ax.plot(x_arr, data_arr[i, :], label=f"{label}, nt={i}", linewidth=2)
+    elif propname.lower() in v_map:
+        nx = np.shape(ds.variables["v"][:].data)[-2]
+        nt = np.shape(ds.variables["v"][:].data)[0]
+        x_arr = np.linspace(1, nx, nx)
 
-ds.close()
-    
+        data_arr = np.asarray(ds.variables["v"][:].data[:, 0, :, v_map[propname]])
+        voffset = np.linspace(-500, 500, nx)
+        for i in range(startt,nt):
+            ax.plot(x_arr, data_arr[i, :] - voffset, label=f"{label}, nt={i}", linewidth=2)
+        # ax.plot(ds.variables["timestep"][:].data, ds.variables["v"][:].data[:, 0, 0, v_map[propname]], label=label, linewidth=2)
+
+    ds.close()
+        
 ax.legend(fontsize=legend_size, ncol=2, framealpha=1.0)
 
 ax.grid()
 ax.tick_params(axis='both', labelsize=tick_size)
 
-ax.set_xlabel(r"Timestep", fontsize=label_size)
+ax.set_xlabel(r"x", fontsize=label_size)
 ax.set_ylabel(f"{propname}", fontsize=label_size)
 
 fig.savefig(f"{args.plotname}", bbox_inches="tight")
