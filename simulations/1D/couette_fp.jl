6413ef359e5744d79d9f65b40b565a90e7a170b6
Georgii Oblapenko <kunstmord@kunstmord.com>
2025-10-09 11:44:53 +0200

    don't need collision factors here

--- simulations/1D/couette_fp.jl
+++ simulations/1D/couette_fp.jl
@@ -33,7 +33,6 @@ function run(seed, T_wall, v_wall, L, ndens, nx, ppc, Δt, output_freq, n_timest
                                                       species_data, ndens, T_wall, Fnum)
 
     # create collision structs
-    collision_factors = create_collision_factors_array(1, grid.n_cells)
     collision_data_fp = CollisionDataFP(ppc * 2)
     
     # create struct for computation of physical properties
@@ -48,12 +47,6 @@ function run(seed, T_wall, v_wall, L, ndens, nx, ppc, Δt, output_freq, n_timest
     # create struct for second netCDF, this one is for time-averaged 
     ds_avg = NCDataHolder("avg_couette_FP_$(L)_$(nx)_$(v_wall)_$(T_wall)_$(ppc)_after$(avg_start).nc",
                           species_data, phys_props)
-
-    # init collision structs
-    for cell in 1:grid.n_cells
-        collision_factors[1, 1, cell].sigma_g_w_max = estimate_sigma_g_w_max(interaction_data[1,1],
-                                                                             species_data[1], T_wall, Fnum)
-    end
 
     # compute and write data at t=0
     compute_props!(particles, pia, species_data, phys_props)
