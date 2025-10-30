97cbf3d796a0a7e2a6589aa68808b03f87c1d9d8
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2025-10-30 17:04:32 +0100

    full test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,28 +9,28 @@ using StableRNGs
 using LinearAlgebra
 using ChunkSplitters
 
-# include("test_indexing.jl")  # first we test indexing routines
-# include("test_constants.jl")  # test constants
-# include("test_species_data.jl")  # test loading of species data
-# include("test_computes.jl")  # we test functions that compute physical properties
-# include("test_io.jl")  # we test various I/O routines
-# include("test_sampling.jl")  # then we test sampling functions
-# include("test_grid_sampling.jl")  # then we test grid sampling functions
-# include("test_collision_utils.jl")  # then we test some collision utilities
-# include("test_bkw.jl")  # then we test the BKW relaxation case against reference solutions
-# include("test_2species.jl")  # then we test 2-species elastic collisions
-# include("test_merging_grid_indexing.jl")  # then we test basic indexing in grid-based merging
-# include("test_merging_grid_merging.jl")  # then we test grid-based merging in 0D
-# include("test_merging_grid_buffer_sorting.jl")  # then we test that particles are freed correctly in NNLS-based merging
-# include("test_bkw_varweight_grid.jl")  # then we test variable-weight NTC collisions+grid-based merging in 0D (BKW relaxation)
-# include("test_octree_bounds_and_splitting.jl")  # then we test bin bounds and splitting in octree merging
-# include("test_octree_sorting.jl")  # then we test sorting in octree merging
-# include("test_octree_merging.jl")  # then we test computation of props and octree merging
-# include("test_octree_merging_buffer_sorting.jl")  # then we test that particles are freed correctly in octree merging
-# include("test_bkw_varweight_octree.jl")  # then we test variable-weight NTC collisions+octree merging in 0D (BKW relaxation)
-# include("test_2species_varweight_octree.jl")  # 2-species variable-weight elastic collisions and merging
-# include("test_nnls_utils.jl")  # test NNLS-based merging utils first
-# include("test_nnls_merging.jl")  # test NNLS-based merging
+include("test_indexing.jl")  # first we test indexing routines
+include("test_constants.jl")  # test constants
+include("test_species_data.jl")  # test loading of species data
+include("test_computes.jl")  # we test functions that compute physical properties
+include("test_io.jl")  # we test various I/O routines
+include("test_sampling.jl")  # then we test sampling functions
+include("test_grid_sampling.jl")  # then we test grid sampling functions
+include("test_collision_utils.jl")  # then we test some collision utilities
+include("test_bkw.jl")  # then we test the BKW relaxation case against reference solutions
+include("test_2species.jl")  # then we test 2-species elastic collisions
+include("test_merging_grid_indexing.jl")  # then we test basic indexing in grid-based merging
+include("test_merging_grid_merging.jl")  # then we test grid-based merging in 0D
+include("test_merging_grid_buffer_sorting.jl")  # then we test that particles are freed correctly in NNLS-based merging
+include("test_bkw_varweight_grid.jl")  # then we test variable-weight NTC collisions+grid-based merging in 0D (BKW relaxation)
+include("test_octree_bounds_and_splitting.jl")  # then we test bin bounds and splitting in octree merging
+include("test_octree_sorting.jl")  # then we test sorting in octree merging
+include("test_octree_merging.jl")  # then we test computation of props and octree merging
+include("test_octree_merging_buffer_sorting.jl")  # then we test that particles are freed correctly in octree merging
+include("test_bkw_varweight_octree.jl")  # then we test variable-weight NTC collisions+octree merging in 0D (BKW relaxation)
+include("test_2species_varweight_octree.jl")  # 2-species variable-weight elastic collisions and merging
+include("test_nnls_utils.jl")  # test NNLS-based merging utils first
+include("test_nnls_merging.jl")  # test NNLS-based merging
 include("test_nnls_merging_buffer_sorting.jl")  # then we test that particles are freed correctly in NNLS-based merging
 include("test_bkw_varweight_nnls.jl")  # test NNLS-based merging
 include("test_electron_neutral_data.jl")  # test loading of XML LXCat-like data
