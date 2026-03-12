4289fc4df8c604aadca4c6636c01a3fd9bacf054
Georgii Oblapenko <kunstmord@kunstmord.com>
2026-03-12 21:42:25 +0100

    BKW NNLS setup

--- simulations/0D/BKW/bkw_varweight_nnls.jl
+++ simulations/0D/BKW/bkw_varweight_nnls.jl
@@ -25,18 +25,30 @@ using ..Merzbild
 using Random
 using TimerOutputs
 
-function run(seed, n_up_to_total, n_full_up_to_total, threshold, ntarget_octree)
+"""
+    run(seed, n_full_up_to_total, threshold, ntarget_octree)
+
+Run the BKW simulation with NNLS merging.
+
+Positional arguments:
+* `seed`: random seed value
+* `n_full_up_to_total`: all mixed moments up to this order are conserved during merging
+* `threshold`: threshold number of particles after which they are merged
+* `ntarget_octree`: in case NNLS merging fails and backup NNLS merging fails, resorting
+to octree merging with this target number of particles
+"""
+function run(seed, n_full_up_to_total, threshold, ntarget_octree)
     Random.seed!(seed)
     rng::Xoshiro = Xoshiro(seed)
 
-    reset_timer!()
+    # reset_timer!()
     species_data::Vector{Species} = load_species_data("data/particles.toml", "Ar")
     interaction_data::Array{Interaction, 2} = load_interaction_data("data/pseudo_maxwell.toml", species_data)
 
     dt_scaled = 0.025
-    n_t = 500
+    n_t = 600
 
-    nv = 32
+    nv = 36
     np_base = 40^3  # some initial guess on # of particle in simulation
 
     mim = []
@@ -45,13 +57,15 @@ function run(seed, n_up_to_total, n_full_up_to_total, threshold, ntarget_octree)
         append!(mim, compute_multi_index_moments(i))
     end
 
-    for i in n_full_up_to_total+1:n_up_to_total
-        append!(mim, [[i, 0, 0], [0, i, 0], [0, 0, i]])
+    mim_backup = []
+    n_moms_backup = n_full_up_to_total - 1
+    for i in 1:n_moms_backup
+        append!(mim_backup, compute_multi_index_moments(i))
     end
-    println(length(mim))
 
-    @timeit "NNLSinit" mnnls = NNLSMerge(mim, threshold)
-    @timeit "OctreeInit" ocm = OctreeN2Merge(OctreeBinMidSplit; init_bin_bounds=OctreeInitBinMinMaxVel, max_Nbins=6000)
+    mnnls = NNLSMerge(mim, threshold)
+    mnnls_backup = NNLSMerge(mim_backup, threshold)
+    ocm = OctreeN2Merge(OctreeBinMidSplit; init_bin_bounds=OctreeInitBinMinMaxVel, max_Nbins=6000)
 
     T0::Float64 = 273.0
     sigma_ref = π * (interaction_data[1,1].vhs_d^2)
@@ -68,42 +82,37 @@ function run(seed, n_up_to_total, n_full_up_to_total, threshold, ntarget_octree)
 
     n_sampled = sample_on_grid!(rng, vdf0, particles[1], nv, species_data[1].mass, T0, n_dens,
                                 0.0, 1.0, 0.0, 1.0, 0.0, 1.0;
-                                v_mult=3.5, cutoff_mult=3.5, noise=0.0, v_offset=[0.0, 0.0, 0.0])
+                                v_mult=4.0, cutoff_mult=4.0, noise=0.0, v_offset=[0.0, 0.0, 0.0])
 
     pia = ParticleIndexerArray(n_sampled)
 
     phys_props::PhysProps = PhysProps(1, 1, moments_list, Tref=T0)
     compute_props_with_total_moments!(particles, pia, species_data, phys_props)
 
-    ds = NCDataHolder("scratch/data/tmp_nnls_$(n_full_up_to_total)full_upto$(n_up_to_total)_$(threshold)_$(seed).nc", species_data, phys_props)
+    ds = NCDataHolder("scratch/data/bkw_nnls_$(n_full_up_to_total)full_$(threshold)_$(seed).nc", species_data, phys_props)
 
     write_netcdf(ds, phys_props, 0)
 
-    Fnum = n_dens/n_sampled
-
-    firstm = true
-
     if phys_props.np[1,1] > threshold
-        if firstm
-            @timeit "NNLSmerge: 1st time" nnls_success_flag = merge_nnls_based!(rng, mnnls, particles[1], pia, 1, 1;
-                                                                                vref=vref, scaling=:vref)
-            firstm = false
-        else
-            @timeit "NNLSmerge" nnls_success_flag = merge_nnls_based!(rng, mnnls, particles[1], pia, 1, 1;
-                                                                        vref=vref, scaling=:vref)
-        end
+        nnls_success_flag = merge_nnls_based!(rng, mnnls, particles[1], pia, 1, 1;
+                                                                            vref=vref, scaling=:variance, centered_at_mean=false, v_multipliers=[], iteration_mult=4)
 
         if nnls_success_flag == -1
-            println("Resorting to octree merging on timestep $ts")
-            @timeit "Octreemerge" merge_octree_N2_based!(rng, ocm, particles[1], pia, 1, 1, ntarget_octree)
+            nnls_success_flag = merge_nnls_based!(rng, mnnls_backup, particles[1], pia, 1, 1;
+                                                                    vref=vref, scaling=:variance, centered_at_mean=false, v_multipliers=[], iteration_mult=4)
+            
+            if nnls_success_flag == -1
+                merge_octree_N2_based!(rng, ocm, particles[1], pia, 1, 1, ntarget_octree)
+            end
         end
-
-        Fnum = n_dens / pia.n_total[1]  # update Fnum estimate
     end
 
     collision_factors::CollisionFactors = CollisionFactors()
     collision_data::CollisionData = CollisionData()
 
+    # average Fnum is: 
+    # Fnum = n_dens/n_sampled
+    Fnum = n_dens/pia.indexer[1,1].n_local
     collision_factors.sigma_g_w_max = estimate_sigma_g_w_max(interaction_data[1,1], species_data[1], T0, Fnum)
 
     Δt::Float64 = dt_scaled * tref
@@ -111,43 +120,55 @@ function run(seed, n_up_to_total, n_full_up_to_total, threshold, ntarget_octree)
 
     nnls_success_flag = 1
 
+    # npp = 0.0
+
     for ts in 1:n_t
-        if ts % 100 == 0
-            println(ts)
-        end
+        # if ts % 100 == 0
+        #     println(ts)
+        # end
 
         ntc!(rng, collision_factors, collision_data, interaction_data, particles[1], pia, 1, 1, Δt, V)
 
-        if phys_props.np[1,1] > threshold
-            if firstm
-                @timeit "NNLSmerge: 1st time" nnls_success_flag = merge_nnls_based!(rng, mnnls, particles[1], pia, 1, 1;
-                                                                                    vref=vref, scaling=:vref)
-                firstm = false
-            else
-                @timeit "NNLSmerge" nnls_success_flag = merge_nnls_based!(rng, mnnls, particles[1], pia, 1, 1;
-                                                                          vref=vref, scaling=:vref)
-            end
-
+        # npp += pia.indexer[1,1].n_local
+        if pia.indexer[1,1].n_local > threshold
+            nnls_success_flag = merge_nnls_based!(rng, mnnls, particles[1], pia, 1, 1;
+                                                                      vref=vref, scaling=:variance, centered_at_mean=false, v_multipliers=[], iteration_mult=4)
             if nnls_success_flag == -1
-                println("Resorting to octree merging on timestep $ts")
-                @timeit "Octreemerge" merge_octree_N2_based!(rng, ocm, particles[1], pia, 1, 1, ntarget_octree)
+                nnls_success_flag = merge_nnls_based!(rng, mnnls_backup, particles[1], pia, 1, 1;
+                                                                      vref=vref, scaling=:variance, centered_at_mean=false, v_multipliers=[], iteration_mult=4)
+                
+                if nnls_success_flag == -1
+                    merge_octree_N2_based!(rng, ocm, particles[1], pia, 1, 1, ntarget_octree)
+                end
             end
         end
+
+        squash_pia!(particles, pia, 1)
         
         compute_props_with_total_moments!(particles, pia, species_data, phys_props)
         write_netcdf(ds, phys_props, ts)
     end
     close_netcdf(ds)
-    print_timer()
 end
 
-# run(1, 5, 3, 50, 30)
-run(0, 8, 6, 150, 100)
+# run with merging conserving all mixed moments up to order 6; merging is called when particle count exceeds 100
+# octree merging with 85 target particles is used when all else fails
+run(0, 6, 100, 85)
 
-# multiple runs with ensembling if needed
-# const params = [[5, 3, 50, 30], [6, 4, 75, 50], [7, 5, 100, 70], [8, 6, 150, 100], [9, 7, 200, 140], [10, 8, 250, 170]]
-# for paramset in params
-#     for seed in 101:400
-#         run(seed, paramset[1], paramset[2], paramset[3], paramset[4])
+
+#  # Uncomment set-up below to run over the parameter sets used for "Moment-preserving particle merging via non-negative least squares"
+#  # each element of params is a 4-element vector: first element is the n_full_up_to_total parameter (maximum order of moments conserved)
+#  # second is the threshold number of particles
+#  # third is the target number of particles for the backup octree merging
+#  # fourth is the number of ensembles that are run with different random seeds 
+# const params = [[4, 50, 36, 10800], [5, 66, 55, 4800], [6, 100, 85, 2160], [7, 142, 120, 1040], [8, 200, 164, 560], [9, 264, 220, 400]]
+# const nseeds = [10800, 4800, 2160, 1040, 560, 400]
+# for (paramset, ns) in zip(params, nseeds)
+#     println("$paramset with $ns seeds")
+#     for seed in 1:ns
+#         if seed % 100 == 0
+#             println(seed, ", ", paramset)
+#         end
+#         run(seed, paramset[1], paramset[2], paramset[3])
 #     end
 # end
