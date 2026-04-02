34b1d937412cee8a2c7e4210de677f465a055062
Georgii Oblapenko <kunstmord@kunstmord.com>
2026-04-02 09:04:34 +0200

    references update

--- src/merging/merging_nnls.jl
+++ src/merging/merging_nnls.jl
@@ -1447,8 +1447,8 @@ the number of non-zero (or smaller than `w_threshold`) elements in the solution 
 `-1` is returned to signify a failure of the merging algorithm.
 
 # References
-* G. Oblapenko, A Non-Negative Least Squares-based Approach for Moment-Preserving Particle Merging.
-    [arXiv preprint, 2025](https://doi.org/10.48550/arXiv.2412.12354).
+* G. Oblapenko, M. Torrilhon, Moment-preserving particle merging via non-negative least squares.
+    [arXiv preprint, 2026](https://doi.org/10.48550/arXiv.2604.00668).
 """
 function merge_nnls_based!(rng, nnls_merging, particles, pia, cell, species;
                            vref=1.0, scaling=:variance,
@@ -1572,8 +1572,8 @@ the number of non-zero (or smaller than `w_threshold`) elements in the solution 
 `-1` is returned to signify a failure of the merging algorithm.
 
 # References
-* G. Oblapenko, A Non-Negative Least Squares-based Approach for Moment-Preserving Particle Merging.
-    [arXiv preprint, 2025](https://doi.org/10.48550/arXiv.2412.12354).
+* G. Oblapenko, M. Torrilhon, Moment-preserving particle merging via non-negative least squares.
+    [arXiv preprint, 2026](https://doi.org/10.48550/arXiv.2604.00668).
 """
 function merge_nnls_based_rate_preserving!(rng, nnls_merging,
                                            interaction, electron_neutral_interactions, computed_cs,
@@ -1704,8 +1704,8 @@ the number of non-zero (or smaller than `w_threshold`) elements in the solution 
 `-1` is returned to signify a failure of the merging algorithm.
 
 # References
-* G. Oblapenko, A Non-Negative Least Squares-based Approach for Moment-Preserving Particle Merging.
-    [arXiv preprint, 2025](https://doi.org/10.48550/arXiv.2412.12354).
+* G. Oblapenko, M. Torrilhon, Moment-preserving particle merging via non-negative least squares.
+    [arXiv preprint, 2026](https://doi.org/10.48550/arXiv.2604.00668).
 """
 function merge_nnls_based_rate_preserving!(rng, nnls_merging,
                                            interaction, electron_neutral_interactions, computed_cs,
