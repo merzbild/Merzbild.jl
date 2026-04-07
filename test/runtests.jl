537b30e6732ddfcae432f5f6b34a6ed494bc3be3
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2026-04-07 16:49:38 +0200

    tests for new collision function

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,6 +18,7 @@ include("test_sampling.jl")  # then we test sampling functions
 include("test_grid_sampling.jl")  # then we test grid sampling functions
 include("test_phase_box_sampling.jl")  # then we test phase box sampling
 include("test_collision_utils.jl")  # then we test some collision utilities
+include("test_collision_vhs.jl")  # then we test VHS collisions
 include("test_bkw.jl")  # then we test the BKW relaxation case against reference solutions
 include("test_2species.jl")  # then we test 2-species elastic collisions
 include("test_merging_grid_indexing.jl")  # then we test basic indexing in grid-based merging
