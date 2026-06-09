797ae5131189f0a1e00b22e5a3673b7a4cee4e56
Georgii Oblapenko <kunstmord@kunstmord.com>
2026-06-09 21:44:24 +0200

    fixed setting of index_last during particle deletion

--- src/particles.jl
+++ src/particles.jl
@@ -512,6 +512,9 @@ If no particles are present in the 1st group of particles, the function does not
                     end
                 end
             end
+            if !found
+                @inbounds pia.index_last[species] = 0
+            end
         end
     end
 
@@ -575,15 +578,20 @@ If no particles are present in the 2nd group of particles, the function does not
                 end
             end
 
-            # if we start searching for group1, we need to include current cell!
+            # if we start searching for group1, we need to include all cells!
+            @inbounds n_cells = size(pia.indexer)[1]
+
             if !found
-                for c in cell:-1:1
+                for c in n_cells:-1:1
                     @inbounds if pia.indexer[c, species].n_group1 > 0
                         @inbounds pia.index_last[species] = pia.indexer[c, species].end1
                         found = true
                         break
                     end
                 end
+            end
+            if !found
+                @inbounds pia.index_last[species] = 0
             end
         end
     end
