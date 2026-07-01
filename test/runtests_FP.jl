94d76eccb3fa524731a9426bdb5826d7f57c1d78
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2026-07-01 17:16:39 +0200

    tests for linear FP, fixed-weight

--- test/runtests_FP.jl
+++ test/runtests_FP.jl
@@ -1,1 +1,2 @@
-include("test_collision_fp.jl")  # test Fokker-Planck collisions
+include("test_collision_fp.jl")  # test Fokker-Planck collisions: sampling and scaling random numbers
+include("test_linear_fp.jl")  # test linear FP operator
