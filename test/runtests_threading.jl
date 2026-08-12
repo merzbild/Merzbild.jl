b63d5cd97d0c157d37a981c53ee8d46dd2b8f4cc
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2026-08-12 16:58:26 +0200

    rebalancing tests

--- test/runtests_threading.jl
+++ test/runtests_threading.jl
@@ -2,3 +2,4 @@ include("test_chunking.jl")  # test chunking, chunk-based computes, surface redu
 include("test_particle_exchange.jl")  # test swapping and pushing of particles in chunked simulations
 include("test_particle_resort_after_exchange.jl")  # test re-sorting of particles after swap/push in chunked simulations
 include("test_1_factorization.jl")  # test thread-safe 1-factorization of list of chunk pairs
+include("test_rebalance.jl")  # test rebalancing
