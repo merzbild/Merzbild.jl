144a84762ac78d908daf925b73571dea7324bae6
Georgii Oblapenko <kunstmord@kunstmord.com>
2026-08-18 12:44:03 +0200

    add rebalance frequency parameter

--- simulations/1D/couette_multithreaded.jl
+++ simulations/1D/couette_multithreaded.jl
@@ -238,4 +238,5 @@ end
 
 const n_t = 50000
 run(1234, 300.0, 500.0, 5e-4, 5e22, 2000, 250, 2.59e-9, 1000, n_t, 14000;
-    chunk_count_multiplier=1, preallocation_margin_multiplier=1.5, parallel_exchange=false, final_debug=true)
+    chunk_count_multiplier=1, preallocation_margin_multiplier=1.5, parallel_exchange=false, final_debug=true,
+    rebalance_freq=2500)
