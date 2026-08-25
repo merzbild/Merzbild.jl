ab644cd19b116a758f753e17ebddba750e81b995
Georgii Oblapenko <kunstmord@kunstmord.com>
2026-08-25 12:46:32 +0200

    abstract Poisson BC1D type

--- src/abstract_types.jl
+++ src/abstract_types.jl
@@ -19,3 +19,11 @@ abstract type AbstractNCDataHolder end
 Abstract type for boundary conditions
 """
 abstract type AbstractBC end
+
+"""
+    AbstractFieldBC1D
+
+Abstract type for boundary conditions imposed on the electrostatic field in 1-D simulations.
+Not to be confused with [`AbstractBC`](@ref), which describes particle-surface interaction.
+"""
+abstract type AbstractFieldBC1D end
